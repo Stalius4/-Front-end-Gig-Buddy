d23ebebd3b6c9e2f4f8b674981132883eea5f4ee
Richard <isit_ric@hotmail.com>
2022-07-08 10:19:25 +0100

    commit

--- src/pages/LogorSign/LogorSign.js
+++ src/pages/LogorSign/LogorSign.js
@@ -35,12 +35,13 @@ export const LogOrSign = ({user, setUser}) => {
     try { 
         // switch between log in page and sign up page
         const [logSwitch, setlogSwitch] = useState(true);
+
         return(
-        <>
+        <div className="bg-image">
             
                 { logSwitch ?
-        <div className="bg-image">
-            {/* <h1 className="gigbuddy-logorsign-title">Gig Buddy</h1> */}
+      
+        
             <div className="content-flex">
             <div className="form-container">
             <h1 className="gigbuddy-logorsign-title">Gig Buddy</h1>
@@ -53,20 +54,25 @@ export const LogOrSign = ({user, setUser}) => {
                     <button className="form-btn">Submit</button>
                     <h3 className="small-text">Don't have an account?</h3>
                     <button className = "form-btn" onClick={(e)=>{setlogSwitch()}}>Sign Up</button>
+
+
                 </form>
                 </div>
+
                     <div className="description-container">
-                        <h2 className="page-description">People going to gigs, but do not have friends with similar music taste and therefore have to go alone</h2>
+                        <h2 className="page-description">Find Music Find Friends</h2>
                     </div>
+
+                        <h2 className="page-description">People going to gigs, but do not have friends with similar music taste and therefore have to go alone</h2>
                 </div>
-            </div>
+          
                 :
-            <div className="bg-image">
-                {/* <h1 className="gigbuddy-logorsign-title">Gig Buddy</h1> */}
+           
+              
                 <div className="content-flex">
                 <section className="form-container">
                 <h1 className="gigbuddy-logorsign-title">Gig Buddy</h1>
-                <form className="login-form" onSubmit={submitHandlerCreate}>
+                <form className="login-form" onSubmit={submitHandlerLogin}>
                     <h1 className="find-buddy-text">Find your Gig Buddy</h1>
                     <h1 className="text">Create Account</h1>
                     <FaUser className="signlog-icons"/>
@@ -78,16 +84,22 @@ export const LogOrSign = ({user, setUser}) => {
                     <button className="form-btn">Submit</button>
                     <h3 className="small-text">Already have an account?</h3>
                     <button className = "form-btn" onClick={(e)=>{setlogSwitch(true)}}>Log in</button>
+
                 </form>
                 </section>
 
+
                     <div className="description-container">
-                        <h2 className="page-description">People going to gigs, but do not have friends with similar music taste and therefore have to go alone</h2>
+                        <h2 className="page-description">Find Music Find Friends</h2>
                     </div>
+
+                   
+                        <h2 className="page-description">People going to gigs, but do not have friends with similar music taste and therefore have to go alone</h2>
                 </div>
-            </div>
+       
                 } 
-                </>
+                </div>
+
         )
     } catch (error){ 
         console.log (error)
